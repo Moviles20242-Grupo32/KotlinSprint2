a7e359a75a80a1c671a7fa19474431c975d39e61
Are1205 <andresarevalo1205@gmail.com>
2024-09-19 18:52:58 -0500

    Home MVVM and Service Adapter
    - Creation Item Model
    - Creation Service Adapter Model
    - Service adapter Fire Base Connection and Get all items
    - ViewModelHome and connect with home screen
    - Home Screen declarations and implementation without location component

--- app/src/main/java/com/example/foodies/MainActivity.kt
+++ app/src/main/java/com/example/foodies/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.foodies.navigation.FoodiesNavigation
-import com.example.foodies.navigation.FoodiesScreens
 import com.example.foodies.ui.theme.FoodiesTheme
 
 
